56be9c411fe618b9158a149e648f493da9a42f98
Jeremy Baker <jhubert@gmail.com>
2013-03-27 16:18:31 -0700

    Use osascript instead of touch command
    For some reason, touching the .AccessibilityAPIEnabled file doesn't seem to enable Assistive Devices like it should. It marks it as enabled in the preferences, but the window manager doesn't actually work. Apple Script seems to fix that, but has the downside of requiring the users password during the execution.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,15 +4,17 @@
 #
 #     include spectacle
 class spectacle {
-  package { 'Spectacle':
+  package { 'spectacle':
     source   => 'https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.1.zip',
     provider => 'compressed_app'
   }
 
+
   exec { "enable assistive devices":
-    command => "echo -n 'a' | tee /private/var/db/.AccessibilityAPIEnabled > /dev/null 2>&1; chmod 444 /private/var/db/.AccessibilityAPIEnabled",
+    command => "osascript -e 'tell application \"System Events\" to set UI elements enabled to true'",
     onlyif => "[ ! -f /private/var/db/.AccessibilityAPIEnabled ]",
-    user => root
+    user => root,
+    require => Package['spectacle']
   }
 
 }
